1de85778b17a1eae6960c9fb4d2b29333deb26d3
Jari Heikkinen <146705481+jahetahe@users.noreply.github.com>
2024-04-13 09:43:15 +0300

    Update Ultrasound, working version

--- Python/Ultrasound.py
+++ Python/Ultrasound.py
@@ -4,8 +4,8 @@ import time
 SOUND_SPEED=340
 TRIG_PULSE_DURATION_US=10
 
-trigger = Pin(10, Pin.OUT) # pin 10 in Pico
-echo = Pin(9, Pin.IN)  # pin 9 in Pico
+trigger = Pin("GP7", Pin.OUT) # pin 10 in Pico
+echo = Pin("GP6", Pin.IN)  # pin 9 in Pico
 
 while True:
     trigger.value(0)
@@ -16,7 +16,5 @@ while True:
 
     ultrason_duration = time_pulse_us(echo, 1, 30000)
     distance_cm = SOUND_SPEED * ultrason_duration / 20000
-    print("Trigger pin value:", trigger.value())
-    print("Echo Pin value:", echo.value())
     print(f"Distance : {distance_cm} cm")
     time.sleep_ms(500)
